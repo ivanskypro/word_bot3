e494eb2aa01130ec7e97bd4e1b6d6f09b5a1eb0e
ivanskypro <kalmych@gmail.com>
2023-03-28 21:25:05 +0300

    Initial commit

---
+++ src/main/java/com/dolan/namruev/kemal/word_bot/TelegramBotApplication.java
@@ -0,0 +1,15 @@
+package com.dolan.namruev.kemal.word_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class TelegramBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelegramBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dolan/namruev/kemal/word_bot/TelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dolan.namruev.kemal.word_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
