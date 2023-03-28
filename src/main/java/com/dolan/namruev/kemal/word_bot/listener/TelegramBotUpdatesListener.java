7779e0eb66a547510cf0add896f4c57376e68426
ivanskypro <kalmych@gmail.com>
2023-03-28 21:55:17 +0300

    1.

--- src/main/java/com/dolan/namruev/kemal/word_bot/listener/TelegramBotUpdatesListener.java
+++ src/main/java/com/dolan/namruev/kemal/word_bot/listener/TelegramBotUpdatesListener.java
@@ -3,6 +3,7 @@ package com.dolan.namruev.kemal.word_bot.listener;
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
 import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.request.SendMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,7 +15,7 @@ import java.util.List;
 @Service
 public class TelegramBotUpdatesListener implements UpdatesListener {
 
-    private Logger logger = LoggerFactory.getLogger(TelegramBotUpdatesListener.class);
+    private final Logger logger = LoggerFactory.getLogger(TelegramBotUpdatesListener.class);
 
     @Autowired
     private TelegramBot telegramBot;
@@ -28,7 +29,15 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
     public int process(List<Update> updates) {
         updates.forEach(update -> {
             logger.info("Processing update: {}", update);
-            // Process your updates here
+            String helloText = "Ням-ням!";
+            Long chatId = update.message().chat().id();
+            String messageText = update.message().text();
+            if (update.message().text() != null) {
+                if (messageText.equals("/start")) {
+                    SendMessage message = new SendMessage(chatId, helloText);
+                    telegramBot.execute(message);
+                }
+            }
         });
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
     }
